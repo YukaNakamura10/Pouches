20e902ba6873d8a87363431b936893e7fb07fe23
YukaNakamura00 <ynakamu@purdue.edu>
2021-10-28 13:55:59 -0400

    second commit

---
+++ Pouches/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pouches
+//
+//  Created by School on 27/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        print("Pouches")
+        
+        
+    }
+   
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pouches/PouchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PouchesApp.swift
+//  Pouches
+//
+//  Created by School on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PouchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
